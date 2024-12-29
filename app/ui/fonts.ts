7f75bb1e632c5d0a841acefac5cf7befcc9f9a48
Konyee11 <konyee.527@gmail.com>
2024-12-29 17:28:30 +0900

    Google Fontsをインポートし、Oswald、Inter、Lusitanaフォントを設定しました

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Oswald, Lusitana, Inter } from "next/font/google";
+
+export const oswald = Oswald({ subsets: ["latin"] });
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const lusitana = Lusitana({
+    weight: ["400", "700"],
+    subsets: ["latin"],
+});
